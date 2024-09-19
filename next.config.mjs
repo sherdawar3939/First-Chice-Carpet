5a716681cc465dd241ce6d2ca170bfb08825c248
sakibsarker <sakib15-3824@diu.edu.bd>
2024-09-19 20:16:11 +0600

    middleware move to src

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  experimental: {
-    appDir: true,
-  },
   images: {
     domains: ["images.pexels.com", "res.cloudinary.com"], // Add the domain of the image source
   },
