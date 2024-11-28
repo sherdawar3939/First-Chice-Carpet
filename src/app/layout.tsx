b321749360e7016f8b106c29d7f5d26787bfb850
Engr sher <sherdawar210945@gmail.com>
2024-11-28 19:05:09 +0400

    removing search console

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,13 +21,6 @@ export default function RootLayout({
     <html lang="en">
       {/* <!-- Google tag (gtag.js) --> */}
       <GoogleTagManager gtmId="G-2R4EVV4KDM" />
-      <head>
-        {/* google search console code */}
-        <meta
-          name="google-site-verification"
-          content="5Sq62wmFfr_NoTOytLUYU0f1Dk0xsamV7gOfW_7jH7k"
-        />
-      </head>
       <body className={inter.className}>
         <AntdRegistry>
           <SessionProvider>{children}</SessionProvider>
