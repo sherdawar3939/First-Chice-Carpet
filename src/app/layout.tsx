5b76d5ea907b27cd449c57123f4fd407f43837c3
Abhishek Sharma <abhisheksharmaword@gmail.com>
2026-03-02 17:02:17 +0530

    Added google analytics and site verification

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,13 +3,16 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { AntdRegistry } from "@ant-design/nextjs-registry";
 import { SessionProvider } from "next-auth/react";
-
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "First Choice General Trading LLC  Dubai",
   description: "Home Interior Design",
+  verification: {
+    google: "tzGhhrkOka3QpWhp7_omdhnoqbYoHmgCU4cKvEjX1FY",
+  },
 };
 
 export default function RootLayout({
@@ -21,6 +24,18 @@ export default function RootLayout({
     <html lang="en">
       {/* <!-- Google tag (gtag.js) --> */}
       <body className={inter.className}>
+        {/* google analytics score */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-M3KB9ZX5X1"
+          strategy="afterInteractive"
+        ></Script>
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-M3KB9ZX5X1');`}
+        </Script>
         <AntdRegistry>
           <SessionProvider>{children}</SessionProvider>
         </AntdRegistry>
