8c91d65413b1cd1ffd3b6bfc41d1839f7867a69b
Abhishek Sharma <abhisheksharmaword@gmail.com>
2026-03-18 16:04:40 +0530

    Add gtm script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,9 +24,31 @@ export default function RootLayout({
           name="google-site-verification"
           content="tzGhhrkOka3QpWhp7_omdhnoqbYoHmgCU4cKvEjX1FY"
         />
+        {/* Google Tag Manager Script */}
+        <Script id="gtm-script" strategy="beforeInteractive">
+          {`
+          (function(w,d,s,l,i){w[l] = w[l] || [];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TKGXZZBP');
+          `}
+        </Script>
       </head>
+      
       {/* <!-- Google tag (gtag.js) --> */}
       <body className={inter.className}>
+
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-TKGXZZBP"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+
         {/* google analytics score */}
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-M3KB9ZX5X1"
